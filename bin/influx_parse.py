5b3e0111a8efc47643cbed95a8ff84ff843ee740
Doug Arnold <d.m.arnold@ljmu.ac.uk>
2019-05-09 13:55:18 +0100

    Expanded influxparse.py and migrate.cfg with Rob Smiths required datums

--- bin/influx_parse.py
+++ bin/influx_parse.py
@@ -20,19 +20,62 @@ with open(IN_FILE) as csvfile:
 
 # Set time in s to microseconds for ingestion into INFLUX_DB
 for i in range (len(data)):
-    # Convert from seconds to microseconds
-    data[i][2] = float(data[i][2]) * 1000000
+    # Convert from seconds to nanoseconds
+    data[i][2] = float(data[i][2]) * 100000000
+
+def datumRead():
+
 
 
 lines = []
 for i in range(len(data)):
     s = "{} ".format(TABLE_NAME)
     if data[i][3] != '' :
-        s += "AZR.POSITION={},".format(data[i][3])
+        s += "AZR.AXIS_POSITION={},".format(data[i][3])
     if data[i][4] != '' :
-        s += "ELR.POSITION={},".format(data[i][4])
+        s += "ELR.AXIS_POSITION={},".format(data[i][4])
     if data[i][5] != '' :
-        s += "CSR.POSITION={},".format(data[i][5])
+        s += "CSR.AXIS_POSITION={},".format(data[i][5])
+    if data[i][6] != '' :
+        s += "AZR.AXIS_ENC_1_COUNT={},".format(data[i][6])
+    if data[i][7] != '' :
+        s += "AZR.AXIS_ENC_2_COUNT={},".format(data[i][7])
+    if data[i][8] != '' :
+        s += "AZR.AXIS_ENC_3_COUNT={},".format(data[i][8])
+    if data[i][9] != '' :
+        s += "AZR.AXIS_ENC_4_COUNT={},".format(data[i][9])
+    if data[i][10] != '' :
+        s += "ELR.AXIS_ENC_1_COUNT={},".format(data[i][10])
+    if data[i][11] != '' :
+        s += "ELR.AXIS_ENC_2_COUNT={},".format(data[i][11])
+    if data[i][12] != '' :
+        s += "TCS.M1_TILT_MIRROR_XI={},".format(data[i][12])
+    if data[i][13] != '' :
+        s += "TCS.M1_TILT_MIRROR_ETA={},".format(data[i][13])
+    if data[i][14] != '' :
+        s += "TCS.M1_SHIFT_FOCAL_PLANE_XI={},".format(data[i][14])
+    if data[i][15] != '' :
+        s += "TCS.M1_SHIFT_FOCAL_PLANE_ETA={},".format(data[i][15])
+    if data[i][16] != '' :
+        s += "TCS.M2_TILT_MIRROR_XI={},".format(data[i][16])
+    if data[i][17] != '' :
+        s += "TCS.M2_TILT_MIRROR_ETA={},".format(data[i][17])
+    if data[i][18] != '' :
+        s += "TCS.M2_SHIFT_FOCAL_PLANE_XI={},".format(data[i][18])
+    if data[i][19] != '' :
+        s += "TCS.M2_SHIFT_FOCAL_PLANE_ETA={},".format(data[i][19])
+    if data[i][20] != '' :
+        s += "MSR.RED_AXIAL_VALVE_FBK={},".format(data[i][20])
+    if data[i][21] != '' :
+        s += "MSR.YEL_AXIAL_VALVE_FBK={},".format(data[i][21])
+    if data[i][22] != '' :
+        s += "MSR.BLU_AXIAL_VALVE_FBK={},".format(data[i][22])
+    if data[i][23] != '' :
+        s += "MSR.RED_AXIAL_VALVE_DRIVE={},".format(data[i][23])
+    if data[i][24] != '' :
+        s += "MSR.YEL_AXIAL_VALVE_DRIVE={}".format(data[i][24])
+    if data[i][25] != '' :
+        s += "MSR.BLU_AXIAL_VALVE_DRIVE={},".format(data[i][25])
 
 
     # Remove any trailing comma
---
+++ bin/sdb_migrate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import configparser
+
+"""
+python script to call Std utility, parse
+and ingest into influx or mysql databases
+"""
+
+# load ini parameters.
+# parse command line arguments which over-rule any ini settings
+# select the first 20 datums in the list
